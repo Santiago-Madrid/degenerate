e57bc3dfd2868472ca0d1c8c9792c5cc4cfaa2bb
Santiago-Madrid <jhoansanti1509@gmail.com>
2026-03-13 09:00:33 -0500

    feat: Implementacion create products

--- src/main/java/com/company/degenerate/dto/ProductRequestDTO.java
+++ src/main/java/com/company/degenerate/dto/ProductRequestDTO.java
@@ -1,4 +1,17 @@
 package com.company.degenerate.dto;
 
+import lombok.Data;
+
+@Data
 public class ProductRequestDTO {
+    
+    private String name_product;
+    
+    private float price;
+    
+    private String section;
+    
+    private Long stock;
+
+    
 }
--- src/main/java/com/company/degenerate/entity/Product.java
+++ src/main/java/com/company/degenerate/entity/Product.java
@@ -1,5 +1,33 @@
 package com.company.degenerate.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Id;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "product")
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "code_product")
+    private Long code_product;
+
+    @Column(name = "name_product")
+    private String name_product;
+
+    @Column(name = "price")
+    private float price;
+    
+    @Column(name = "section")
+    private String section;
+    
+    
+    @Column(name = "stock")
+    private Long stock;
     
 }
